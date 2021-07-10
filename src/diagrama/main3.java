753ad033d3c239d712f24934af625264dcdc1738
usuario <usuario@DESKTOP-3RHDJ3H>
2021-07-09 19:45:10 -0500

    primero

---
+++ src/diagrama/main3.java
@@ -0,0 +1,18 @@
+package diagrama.de.flujo;
+
+public class main3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		//imprimir la tabla del 7 multiplicado hasta el 12
+		
+		int n = 7;
+		System.out.println("Tabla del 7");
+		
+		for(int i=0 ; i==12 ; i++)
+			System.out.println(i + "*" + n+ "=" + i*n);
+		
+	}
+
+}
