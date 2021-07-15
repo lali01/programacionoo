f293eb22fedb754a04800e82666b97807ee549c7
usuario <usuario@DESKTOP-3RHDJ3H>
2021-07-14 20:41:38 -0500

    primer

--- src/diagrama/main4.java
+++ src/diagrama/main4.java
@@ -1,5 +1,21 @@
 package diagrama.de.flujo;
 
+import java.util.Scanner;
+
 public class main4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.print("Ingrese el número de filas: ");
+		Scanner in = new Scanner(System.in);// TECLADO
+		int n = in.nextInt();
+		for (int fila = 1; fila <= n; fila++) {
+			for (int ast = 1; ast <= fila; ast++) {
+				System.out.print("*");
+			}
+			System.out.println();// Salto de linea
+		}
+	}
 
 }
--- src/diagrama/main5.java
+++ src/diagrama/main5.java
@@ -1,9 +1,21 @@
 package diagrama.de.flujo;
 
+import java.util.Scanner;
+
 public class main5 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+
+		System.out.print("Ingrese el número de filas: ");
+		Scanner in = new Scanner(System.in);// TECLADO
+		int n = in.nextInt();
+		for (int fila = 1; fila <= n; fila++) {
+			for (int ast = n; ast >= fila; ast--) {
+				System.out.print("*");
+			}
+			System.out.println();// Salto de linea
+		}
 
 	}
 
