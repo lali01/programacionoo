8465e7f46f20ac473fc868f0af6fc3fa78c4a737
usuario <usuario@DESKTOP-3RHDJ3H>
2021-07-21 18:53:21 -0500

    Tipos de Datos

---
+++ src/tipos/datos/MainTipoDeDatos.java
@@ -0,0 +1,25 @@
+package tipos.datos;
+
+public class MainTipoDeDatos {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		short cantidadEstudiantes = 1000;
+		
+		byte entradaCircuito = 0;
+		
+		int cantCiudadanos = 100000;
+		
+		long cantMundo = 1000000000;
+		
+		float Sueldo = 1200.56F;
+		
+		double saldoBanco = 10000000.67;
+		
+		boolean casado = true;
+		
+		char opcionMenu = ´C´;  
+	}
+
+}
