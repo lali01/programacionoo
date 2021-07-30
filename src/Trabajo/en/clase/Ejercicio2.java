4d53f64974e7e7308ce398d3d8a88c5bdfe392df
usuario <usuario@DESKTOP-3RHDJ3H>
2021-07-30 17:10:44 -0500

    si

--- src/Trabajo/en/clase/Ejercicio2.java
+++ src/Trabajo/en/clase/Ejercicio2.java
@@ -7,51 +7,50 @@ public class Ejercicio2 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		//crear un programa que solicite ingresar un numero solo positivos y asi seguira solicitando 
-		//ingresar un numero hasta que se ingrese el numero(-1), ya no solicitara mas numeros y en ese
-		// momento el programa debe imprimir la suma de los numeros pares ingresados y el promedio solo
+		// crear un programa que solicite ingresar un numero solo positivos y asi
+		// seguira solicitando
+		// ingresar un numero hasta que se ingrese el numero(-1), ya no solicitara mas
+		// numeros y en ese
+		// momento el programa debe imprimir la suma de los numeros pares ingresados y
+		// el promedio solo
 		// de los numeros impares ingresados hasta ese momento
-		
+
 		Scanner in = new Scanner(System.in);
-		
-		int i = 0;
+
+		int i = 1;
 		int sumap = 0;
 		int sumai = 0;
 		int promedioi = 0;
 		int contimpar = 0;
 		int contpar = 0;
 		int n = 0;
-		
-		do{
-		System.out.println("Escribe un número: ");
-		n = in.nextInt();
-		
-		if(n%2==1){
-			sumai = sumai + n;
-			contimpar++;
-			i = -1;
-	
-		}else {
-			sumap = sumap + n;
-			contpar++;
-			
-		}
-		 	i++;
-		 	
-		}while (n!=-1);
-		
-		if (i!=-1) {
-			promedioi = sumai / n; 
-			System.out.println("El promedio de numeros impares es: " + promedioi);
-			System.out.println("La suma de los numeros pares es: " + sumap);
-			
-		}else {
-			
-	}			
-		
+
+		do {
+			System.out.println("Escribe un número: ");
+			n = in.nextInt();
+
+			if (n % 2 == 1) {
+				sumai = sumai + n;
+				contimpar++;
+				
+				if (n % 2 == 0) {
+					sumap = sumap + n;
+					contpar++;
+					
+				}
+
+			} else {
+				contimpar = contimpar + 1;
+				promedioi = sumai/i;
+				
+
+			}
+			i++;
+
+		} while (n != -1);
+		System.out.println("El promedio de numeros impares es: " + promedioi);
+		System.out.println("La suma de los numeros pares es: " + sumap);
+
 	}
+
 }
-
-
-
-
