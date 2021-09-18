5580c2604090943b24807ecf9ee2a8a24713ea67
Stefany Cruz <stefanycruzespinoza@hotmail.com>
2021-09-17 20:14:36 -0500

    list

---
+++ src/ejercicio/coleccion/list/Main.java
@@ -0,0 +1,27 @@
+package ejercicio.coleccion.list;
+
+import java.util.List;
+import java.util.ArrayList;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//ctrl+shift+o importar paquetes
+		
+		Integer numero1 = 1;
+		Integer numero2 = 4;
+		Integer numero3 = 8;
+		Integer numero4 = 20;
+		
+		List<Integer> listaNueva = new ArrayList<Integer>();
+		
+		listaNueva.add(numero1);
+		listaNueva.add(numero2);
+		listaNueva.add(numero3);
+		listaNueva.add(numero4);
+		
+		int vector[] = new int [10];
+	}
+
+}
